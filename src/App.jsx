08b977aba0b8ef76381538926e024a0a76638167
Christian Zuckero <christianazuckero@gmail.com>
2024-04-03 14:00:41 -0400

    Added reusable components

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,11 @@
-import { useState, useEffect } from 'react'
 import './App.css'
+import ContactList from './components/ContactList';
 
 function App() {
-  const [count, setCount] = useState(0)
-  
-  useEffect(() => {
-    console.log("hello from useEffect");
-  }, [count]);
 
   return (
     <>
-      <button onClick={() => setCount((count) => count + 1)}>
-        count is {count}
-      </button>
+      <ContactList />
     </>
   )
 }
