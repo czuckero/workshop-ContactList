5ca257875df1e5dd2a1dc976055087423591a3cc
Christian Zuckero <christianazuckero@gmail.com>
2024-04-03 14:14:47 -0400

    fetched data from API

--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import ContactRow from './ContactRow';
+import { useEffect } from "react";
 
 const dummyContacts = [
   { id: 1, name: "R2-D2", phone: "222-222-2222", email: "r2d2@droids.com" },
@@ -9,6 +10,19 @@ const dummyContacts = [
 
 const ContactList = () => {
   const [contacts, setContacts] = useState(dummyContacts);
+  useEffect(() => {
+    async function fetchContacts() {
+      try {
+        const response = await fetch("https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users");
+        const json = await response.json();
+        console.log(json)
+        setContacts(json)
+      } catch (error) {
+        console.log(error);
+      }
+    }
+    fetchContacts()
+  }, []);
   console.log("Contacts: ", contacts);
 
   return (
